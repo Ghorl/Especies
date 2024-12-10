59229229ef8f1474e91f8d849d62624d4eff6b4d
Ghorl <168112712+Ghorl@users.noreply.github.com>
2024-12-10 17:58:47 -0500

    Add files via upload

--- Copia.py
+++ Copia.py
@@ -3,24 +3,26 @@ from numpy import *
 
 class Species:
   
-  def __init__(self):#Constructor
+  def __init__(self):
   #Leyendo el csv y dandole una variable
    self.datos=pd.read_csv('Index of Species.csv',header=0)
    self.ListofSpecies=[]
 
   def Search_Specie(self):
+
     especie=input("Ingrese el nombre de la especie que desee buscar: ")
-    
-    if especie in self.datos['Species']:
+
+    if especie in self.datos['Species'].values:
       print("Especie encontrada. Agregada a la lista")
       self.ListofSpecies.append(especie)
     else:
        print("Especie no encontrada")
 
   def Report(self):
-     #columns=["Especie"]
-     #df=pd.DataFrame([self.ListofSpecies],columns=columns)  
-     pass
+     columns=["Especie"]
+     df=pd.DataFrame([self.ListofSpecies],columns=columns)
+     print(df)
+  
 
 def Menu():
     especies=Species()
@@ -39,8 +41,6 @@ def Menu():
                 case 3:
                     print("Saliendo del programa.....")
                     break  
-                case _:
-                    print("Opción inválida. Vuelva a intentarlo.")
         except ValueError:
             print("Por favor, ingrese un número válido.")
     
